40c1bcd8e451126e3606912cebf4a195268e15cb
hackerankk <vikassinghkandari1234@gmail.com>
2025-01-03 20:39:07 +0530

    error add nextjsx

---
+++ nextjs/sai2/src/app/page.tsx
@@ -0,0 +1,15 @@
+
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import SupportService from "./components/SupportService";
+
+export default function Home() {
+  return (
+    <>
+    <Hero/>
+    <SupportService/>
+    <Services/>
+    </>
+  );
+}
+
